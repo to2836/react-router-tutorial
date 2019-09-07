784e28642e6e540afe965771320130b6fa4eef4a
KIM JIN HYUK <jinjin2836@gmail.com>
2019-09-07 23:26:24 +0900

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import {Router} from 'react-router-dom';
+import {Home, About} from 'pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Comment {
+  render(){
+    return (
+      <div>
+        <Router exact path="/" component={Home}/>
+        <Router exact path="/about" component={About}/>
+      </div>
+     );
+  }
+  
 }
 
 export default App;
